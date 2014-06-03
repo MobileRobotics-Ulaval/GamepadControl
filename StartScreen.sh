e50228ce97621b8c4e2917568be89fac060dcbfd
smichaud <sebastien.michaud@arcbees.com>
2014-06-03 12:22:15 -0400

    Delete StartScreen.sh

--- StartScreen.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-
-set -e
-set -v
-
-source /opt/ros/fuerte/setup.bash
-export ROS_PACKAGE_PATH="/home/administrator/ros_uLaval":$ROS_PACKAGE_PATH
-
-screen -d -m python /home/administrator/GamepadControl/GamepadDaemon.py
